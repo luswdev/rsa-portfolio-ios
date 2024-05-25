ac234d6e1b203483c7efb99cbec4e21f4d603e1a
LuSkywalker <callumlu@gmail.com>
2024-05-25 17:44:04 +0800

    Initial Commit

---
+++ RSA Portfolio/RSA_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSA_PortfolioApp.swift
+//  RSA Portfolio
+//
+//  Created by Skywalker on 2024/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RSA_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
